59b5fcc3fbffab7ede172374da4dc1c777c2dd8a
dav55566666 <82227374+dav55566666@users.noreply.github.com>
2025-04-01 08:29:22 +0400

    update

--- src/app/features/auth/store/auth.reducers.ts
+++ src/app/features/auth/store/auth.reducers.ts
@@ -1,7 +1,5 @@
 import { createReducer, on, Store } from "@ngrx/store";
 import { AuthActions } from "./auth.actions";
-import { selectUsers, UserType } from "../../users";
-import { inject } from "@angular/core";
 import { AuthState } from "../interfaces";
 
 export const authReducers = (initialState: AuthState) => {
